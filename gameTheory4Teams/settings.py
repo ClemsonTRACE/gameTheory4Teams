79d55ada0548112e1c7b9c94a014ac2fa1a2d9c1
lbarberiscanoni <lorenzo.barberis.canonico@gmail.com>
2019-03-27 20:53:43 -0400

    whitelisted the full url

--- gameTheory4Teams/settings.py
+++ gameTheory4Teams/settings.py
@@ -59,11 +59,11 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = (
     'localhost:3000',
-    "lbarberiscanoni.github.io",
+    "https://lbarberiscanoni.github.io/gt4teams_frontend/#",
 )
 CORS_ORIGIN_REGEX_WHITELIST = (
     'localhost:3000',
-    "lbarberiscanoni.github.io",
+    "https://lbarberiscanoni.github.io/gt4teams_frontend/#",
 )
 
 ROOT_URLCONF = 'gameTheory4Teams.urls'
